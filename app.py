bf7b5f6df741a3d5495c905e52ab177a79d46be8
higoreirilar <higoreirilar@gmail.com>
2026-06-05 15:48:53 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,43 +1,48 @@
 from flask import Flask, request, jsonify
-import psycopg2
 import os
+import psycopg2
+from antifraude import check_fraude
 
 app = Flask(__name__)
 
-def salvar_pedido(ip, valor):
-    conn = psycopg2.connect(
-        dbname="loja",
-        user="postgres",
-        password="@32426022",
-        host="localhost",
-        port="5432"
-    )
+# -------- BANCO --------
+def get_conn():
+    return psycopg2.connect(os.environ["DATABASE_URL"])
 
+def salvar_pedido(ip, valor, status):
+    conn = get_conn()
     cur = conn.cursor()
 
     cur.execute("""
         INSERT INTO pedidos (cliente_id, ip, valor, status)
-        VALUES (1, %s, %s, 'pendente')
-    """, (ip, valor))
+        VALUES (1, %s, %s, %s)
+    """, (ip, valor, status))
 
     conn.commit()
     cur.close()
     conn.close()
 
-@app.route('/compra', methods=['POST'])
-def compra():
-    data = request.json
-
-    ip = data['ip']
-    valor = data['valor']
-
-    salvar_pedido(ip, valor)
-
-    return jsonify({"status": "recebido", "fraude": "em_analise"})
+# -------- ROTAS --------
 
 @app.route("/")
 def home():
-    return "API Cozinet rodando"
+    return "Cozinet API rodando"
+
+@app.route("/compra", methods=["POST"])
+def compra():
+    data = request.json
+
+    ip = data.get("ip")
+    valor = data.get("valor")
+
+    # antifraude
+    resultado = check_fraude(ip, valor)
+
+    status = "bloqueado" if resultado["fraude"] else "aprovado"
+
+    salvar_pedido(ip, valor, status)
+
+    return jsonify(resultado)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))

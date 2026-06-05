5a179abadcfd81474d7556d5508cc49e9183df88
higoreirilar <higoreirilar@gmail.com>
2026-06-05 15:49:18 -0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,50 +1,25 @@
-from flask import Flask, render_template_string
+from flask import Flask, jsonify
+import os
 import psycopg2
 
-app = Flask(__name__)
+dashboard = Flask(__name__)
 
-HTML = """
-<h1>📊 PAINEL ANTIFRAUDE</h1>
-<table border="1" cellpadding="5">
-<tr>
-<th>ID</th>
-<th>IP</th>
-<th>Valor</th>
-<th>Status</th>
-</tr>
-{% for row in data %}
-<tr>
-<td>{{row[0]}}</td>
-<td>{{row[1]}}</td>
-<td>{{row[2]}}</td>
-<td>{{row[3]}}</td>
-</tr>
-{% endfor %}
-</table>
-"""
+def get_conn():
+    return psycopg2.connect(os.environ["DATABASE_URL"])
 
-def get_data():
-    conn = psycopg2.connect(
-        dbname="loja",
-        user="postgres",
-        password="@32426022",
-        host="localhost",
-        port="5432"
-    )
-
+@dashboard.route("/pedidos")
+def pedidos():
+    conn = get_conn()
     cur = conn.cursor()
-    cur.execute("SELECT id, ip, valor, status FROM pedidos ORDER BY id DESC")
-    data = cur.fetchall()
+
+    cur.execute("SELECT * FROM pedidos")
+    dados = cur.fetchall()
 
     cur.close()
     conn.close()
 
-    return data
-
-@app.route("/")
-def home():
-    data = get_data()
-    return render_template_string(HTML, data=data)
+    return jsonify(dados)
 
 if __name__ == "__main__":
-    app.run(port=5001, debug=True)
+    port = int(os.environ.get("PORT", 8001))
+    dashboard.run(host="0.0.0.0", port=port)
